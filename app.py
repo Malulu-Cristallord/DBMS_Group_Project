aaa5b8c029b77a1009ebff9775d0f7f100b990cd
Malulu-Cristallord <malucristallord@gmail.com>
2026-04-20 15:36:31 +0800

    Enable to fetch book data from webpage
    I grabbed the whole data, but at least we can grab the data via our webpage now.

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 import streamlit as st
 
+import book_request
 
-def my_callback():
-    st.session_state.counter += 1
 
-if 'counter' not in st.session_state:
-    st.session_state.counter = 0
+def update_isbn():
+    isbn = st.session_state.user_input
+    book_request.request_book_data(isbn)
+    st.session_state.book_data = book_request.request_book_data_with_returning_value(isbn)
+
+
+if 'book_data' not in st.session_state:
+    st.session_state.counter = ''
 
 st.header("type in the isbn and the system will find the book from open library!")
-st.text_input(label="ISBN", placeholder="Enter ISBN")
-st.button(label="Submit",on_click=my_callback)
+st.text_input(label="ISBN", key='user_input', placeholder="Enter ISBN")
+st.button(label="Submit",on_click=update_isbn)
+st.text_area(label="Book data", key='book_data', placeholder="Will show book data here", height=2000)
+
+# Front end to be edited
--- book_request.py
+++ book_request.py
@@ -2,29 +2,53 @@ import requests
 import sys
 import json
 
-# Fetching data from Open Library API
-user_input = input("Enter the ISBN number: ")
+# Notes: The current db source, Open Library, is still our best choice when it comes to fetching books.
+# However, the db seems to have a major lack of books in Asian language.
+# I will try to use more APIs if possible, and keep them free cause even the biggest dbs I can find still have big holes in them.
+# -Malu
 
-try:
-    book_api = f"https://openlibrary.org/isbn/{user_input}.json"
-    book_response = requests.get(book_api)
-    book_data = book_response.json()
+def request_book_data(user_input):
+    try:
+        book_api = f"https://openlibrary.org/isbn/{user_input}.json"
+        book_response = requests.get(book_api)
+        book_data = book_response.json()
 
-    author_id = book_data["authors"][0]["key"]
-    author_api = f"https://openlibrary.org{author_id}.json"
-    author_response = requests.get(author_api)
-    author_data = author_response.json()
+        author_id = book_data["authors"][0]["key"]
+        author_api = f"https://openlibrary.org{author_id}.json"
+        author_response = requests.get(author_api)
+        author_data = author_response.json()
 
-    # Print the whole JSON for testing
-    print(json.dumps(book_data, indent=4))
-    # Test stat = 9780439362139 (Harry Potter 1)
+        # Print the whole JSON for testing
+        print(json.dumps(book_data, indent=4))
+        # Test stat = 9780439362139 (Harry Potter 1)
 
-except KeyError:
-    sys.exit("Unable to retrieve data. Check the ISBN number.")
+    except KeyError:
+        sys.exit("Unable to retrieve data. Check the ISBN number.")
+
+def request_book_data_with_returning_value(user_input):
+    try:
+        book_api = f"https://openlibrary.org/isbn/{user_input}.json"
+        book_response = requests.get(book_api)
+        book_data = book_response.json()
+
+        author_id = book_data["authors"][0]["key"]
+        author_api = f"https://openlibrary.org{author_id}.json"
+        author_response = requests.get(author_api)
+        author_data = author_response.json()
+
+        return json.dumps(book_data, indent=4)
+        # Test stat = 9780439362139 (Harry Potter 1)
+
+    except KeyError:
+        sys.exit("Unable to retrieve data. Check the ISBN number.")
+
+
+
+
 
 
 # Formatting data
-def format_subjects():
+def format_subjects(book_data):
     try:
         subjects_list = book_data["subjects"]
         subjects_str = " / ".join(subjects_list)
@@ -33,15 +57,11 @@ def format_subjects():
         return "None"
 
 
-def format_dewey():
+def format_dewey(book_data):
     try:
         return book_data["dewey_decimal_class"][0]
     except KeyError:
         return "None"
-
-
-print("Row added.")
-
 
 
 
