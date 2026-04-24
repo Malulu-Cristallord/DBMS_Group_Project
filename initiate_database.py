62f87ac1c95075dd2949850ca4fa0e3c33ff412e
Malulu-Cristallord <malucristallord@gmail.com>
2026-04-24 12:29:51 +0800

    changes to init_db

--- initiate_database.py
+++ initiate_database.py
@@ -1,6 +1,5 @@
 import db_connect
 
-
 def initiate_books():
     query = ("CREATE TABLE IF NOT EXISTS books ("
              "Title            VARCHAR(255) NOT NULL,"
@@ -19,5 +18,16 @@ def initiate_books():
 def initiate_users():
     query = ("CREATE TABLE IF NOT EXISTS users (")
 
+def initiate_bookshelf():
+    query = ("CREATE TABLE IF NOT EXISTS bookshelf (")
 
-initiate_books()
+def del_all():
+    query = ("DROP TABLE *")
+print("Welcome to our group project\nBefore we begin, please make sure that you have a database named \'dbms_group_project\" so that the system can connect\n")
+print("Select your desired function to run, or enter \'full\' for running all initiate db function:\n"
+      "(A) initiate books table\n"
+      "(B) initiate users table\n"
+      "(C) initiate bookshelf table\n"
+      "(N) delete all existing tables (!!Not recommended!!)\n")
+dev_input = input()
+
