5b6888f6959ec7738a0c7cc958a3f1a854c4a0de
Malulu-Cristallord <malucristallord@gmail.com>
2026-04-24 14:02:31 +0800

    add init_posts table

--- initiate_database.py
+++ initiate_database.py
@@ -1,32 +1,46 @@
 import db_connect
 
 def initiate_books():
-    query = ("CREATE TABLE IF NOT EXISTS books ("
-             "Title            VARCHAR(255) NOT NULL,"
-             "Book_ID          INT AUTO_INCREMENT PRIMARY KEY NOT NULL,"
-             "ISBN             VARCHAR(18) NOT NULL,"
-             "Category         VARCHAR(255),"
-             "Publisher        VARCHAR(255),"
-             "Published_Year   YEAR,"
-             "Author           VARCHAR(255),"
-             "Cover            VARCHAR(255),"
-             "Description      VARCHAR(255),"
-             "Rating           DECIMAL)")
+    query = (
+            "CREATE TABLE IF NOT EXISTS books ("
+            "Title            VARCHAR(255) NOT NULL,"
+            "Book_ID          INT AUTO_INCREMENT PRIMARY KEY NOT NULL,"
+            "ISBN             VARCHAR(18) NOT NULL,"
+            "Category         VARCHAR(255),"
+            "Publisher        VARCHAR(255),"
+            "Published_Year   YEAR,"
+            "Author           VARCHAR(255),"
+            "Cover            VARCHAR(255),"
+            "Description      VARCHAR(255),"
+            "Rating           DECIMAL)")
     db_connect.execute_query(query)
 
 
 def initiate_users():
     query = (
             "CREATE TABLE IF NOT EXISTS users ("
-            "id INT AUTO_INCREMENT PRIMARY KEY,"
-            "name VARCHAR(100) NOT NULL,"
-            "email VARCHAR(255) NOT NULL UNIQUE,"
-            "password_hash VARCHAR(255) NOT NULL,"
-            "created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)")
+            "User_ID           INT AUTO_INCREMENT PRIMARY KEY,"
+            "Name              VARCHAR(100) NOT NULL,"
+            "Email             VARCHAR(255) NOT NULL UNIQUE,"
+            "Password_Hash     VARCHAR(255) NOT NULL,"
+            "Created_At        TIMESTAMP DEFAULT CURRENT_TIMESTAMP)")
     db_connect.execute_query(query)
 
 def initiate_bookshelf():
-    query = ("CREATE TABLE IF NOT EXISTS bookshelf (")
+    query = ("CREATE TABLE IF NOT EXISTS bookshelf (") #TBA
+
+def initiate_posts():
+    query = (
+            "CREATE TABLE IF NOT EXISTS posts ("
+            "Title            VARCHAR(255),"
+            "Post_ID          INT AUTO_INCREMENT PRIMARY KEY NOT NULL,"
+            "User_ID          INT REFERENCES Users(User_ID),"
+            "Book_ID          INT REFERENCES Books(Book_ID),"
+            "Upvote_count     INT,"
+            "Created_Date     DATE,"
+            "Rating           SMALLINT)"
+            )
+    db_connect.execute_query(query)
 
 def del_all():
     query = "DROP TABLE IF EXISTS books, users;"
@@ -35,7 +49,7 @@ def del_all():
 def execute_all_methods():
     initiate_books()
     initiate_users()
-
+    initiate_posts()
 
 
 # Run program
@@ -43,14 +57,17 @@ print("Welcome to our group project\nBefore we begin, please make sure that you 
 print("Select your desired function to run, or enter \'full\' for running all initiate db function:\n"
       "(A) initiate books table\n"
       "(B) initiate users table\n"
-      "(C) initiate bookshelf table\n"
+      "(C) initiate posts table\n"
       "(N) delete all existing tables (!!Not recommended!!)\n")
 dev_input = input()
+
 match dev_input:
     case 'A' :
         initiate_books()
     case 'B' :
         initiate_users()
+    case 'C' :
+        initiate_posts()
     case 'N' :
         print("THIS IS NO JOKE, DO YOU REALLY WANT TO DELETE THE WHOLE DATABASE???(type \'affirmative\' to confirm)\ntype any key to stop")
         confirmation_input = input()
