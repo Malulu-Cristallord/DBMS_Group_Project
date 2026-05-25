ae996bad5d58dda851e2020f2aa197e718daff75
Malulu-Cristallord <malucristallord@gmail.com>
2026-05-25 14:05:24 +0800

    add more badges, badge system waiting to be fixed

--- Resources/Badges/initiate_badges.py
+++ Resources/Badges/initiate_badges.py
@@ -9,8 +9,19 @@ badge_read_05 = ("The Reading Games", "Resources/Badges/Badge_Read_05.png", "Rea
 badge_read_06 = ("The Lord of the Books", "Resources/Badges/Badge_Read_06.png", "Read and record 20 reading sessions on LibTrack", "legendary", 500)
 badges_read = (badge_read_01, badge_read_02, badge_read_03, badge_read_04, badge_read_05, badge_read_06)
 
-badge_review_01 = ("Critic's Pen", )
+badge_review_and_post_01 = ("Critic's Pen", "Resources/Badges/Badge_Review&Post_01.png", "Publish your first review on LibTrack", "common", 20)
+badge_review_and_post_02 = ("Critic's Diary", "Resources/Badges/Badge_Review&Post_02.png", "Publish your first post on LibTrack", "common", 20)
+badges_review_and_post = (badge_review_and_post_01, badge_review_and_post_02)
 
+badge_add_books_01 = ("Knowledge Dripping Like a Faucet", "Resources/Badges/Badge_Add_Books_01.png", "Contribute 10 books to the LibTrack Database", "common", 40)
+badge_add_books_02 = ("Knowledge Flowing Like a Shower Head", "Resources/Badges/Badge_Add_Books_02.png", "Contribute 50 books to the LibTrack Database", "uncommon", 100)
+badge_add_books_03 = ("Knowledge Flooding Like a Storm", "Resources/Badges/Badge_Add_Books_03.png", "Contribute 200 books to the LibTrack Database", "rare", 200)
+badge_add_books_04 = ("Knowledge Invading Like a Sea", "Resources/Badges/Badge_Add_Books_04.png", "Contribute 500 books to the LibTrack Database", "legendary", 400)
+badges_add_books = (badge_add_books_01, badge_add_books_02, badge_add_books_03, badge_add_books_04)
+
+all_badges = (badges_read, badges_review_and_post, badges_add_books)
+
+# Waiting for change
 def initiate_badges():
     query = """
     INSERT INTO badges(Badge_Name, Badge_Image_Path, Badge_Description, Badge_Rarity, Badge_Points)
